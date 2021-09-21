5e71bd707e9899a1aca1bac2f1ea22ae34d2c70b
tomasfriz <tomyagustinf@gmail.com>
2021-09-21 12:05:33 -0300

    Terminando el form principal

--- Parcial 1 de laboratorio 2/Friz.Tomas.PrimerParcial/Cibercafe_ElVicio/FrmCibercafe.Designer.cs
+++ Parcial 1 de laboratorio 2/Friz.Tomas.PrimerParcial/Cibercafe_ElVicio/FrmCibercafe.Designer.cs
@@ -29,20 +29,156 @@ namespace Cibercafe_ElVicio
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCibercafe));
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnAyuda
+            // 
+            resources.ApplyResources(this.btnAyuda, "btnAyuda");
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.UseVisualStyleBackColor = true;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.lblUsuario, "lblUsuario");
+            this.lblUsuario.Name = "lblUsuario";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.Name = "label3";
+            // 
+            // lblFecha
+            // 
+            resources.ApplyResources(this.lblFecha, "lblFecha");
+            this.lblFecha.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblFecha.Name = "lblFecha";
+            // 
+            // btnCerrar
+            // 
+            resources.ApplyResources(this.btnCerrar, "btnCerrar");
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Name = "panel1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            // 
+            // Column1
+            // 
+            resources.ApplyResources(this.Column1, "Column1");
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            resources.ApplyResources(this.Column2, "Column2");
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            resources.ApplyResources(this.Column3, "Column3");
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            resources.ApplyResources(this.Column4, "Column4");
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
             // 
             // FrmCibercafe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAyuda);
             this.Name = "FrmCibercafe";
-            this.Text = "Ciberface \"El Vicio\"";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmCibercafe_FormClosing);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     }
 }
 
--- Parcial 1 de laboratorio 2/Friz.Tomas.PrimerParcial/Cibercafe_ElVicio/FrmCibercafe.cs
+++ Parcial 1 de laboratorio 2/Friz.Tomas.PrimerParcial/Cibercafe_ElVicio/FrmCibercafe.cs
@@ -15,7 +15,28 @@ namespace Cibercafe_ElVicio
         public FrmCibercafe()
         {
             InitializeComponent();
+            lblUsuario.Text = "Tomás Agustín Friz";
+            lblFecha.Text = DateTime.Now.ToShortDateString();
         }
 
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Está Seguro que Desea Salir, cerraras todas las ventanas abiertas?", "Cibercafe El Vicio", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnAyuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("El boton 'Ayuda' te ayudara a saber el funcionamiento de los botones", "Cibercafe El Vicio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("El boton 'Cerrar' te preguntara si deseas cerrar. 'SI' cerrara la aplicacion, incluyendo las ventanas abiertas. 'NO' regresaras al menu principal", "Cibercafe El Vicio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void FrmCibercafe_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult msj = MessageBox.Show("¿Seguro de querer salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            e.Cancel = msj == DialogResult.No;
+        }
     }
 }
