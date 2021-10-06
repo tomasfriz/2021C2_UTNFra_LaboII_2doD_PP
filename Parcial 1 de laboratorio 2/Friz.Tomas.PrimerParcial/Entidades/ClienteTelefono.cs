5bf1c4593f6c891fefb82a24cc8009b47169d31d
tomasfriz <tomyagustinf@gmail.com>
2021-10-06 16:29:03 -0300

    Terminado clases y cargado de clientes a una computadora

--- Parcial 1 de laboratorio 2/Friz.Tomas.PrimerParcial/Entidades/ClienteTelefono.cs
+++ Parcial 1 de laboratorio 2/Friz.Tomas.PrimerParcial/Entidades/ClienteTelefono.cs
@@ -188,17 +188,4 @@ namespace Entidades
         }
         #endregion
     }
-
-    //public class ValidadorLlamada : AbstractValidator<ClienteTelefono>
-    //{
-    //    /// <summary>
-    //    /// Valida que los números ingresados.
-    //    /// </summary>
-    //    public ValidadorLlamada()
-    //    {
-    //        RuleFor(x => x.CodigoPais).NotEmpty().MaximumLength(4).Matches("^[0-9]*$").WithMessage("El código no es válido");
-    //        RuleFor(x => x.PrefijoLocalidad).NotEmpty().MinimumLength(2).MaximumLength(5).Matches("^[0-9]*$").WithMessage("El no es válido");
-    //        RuleFor(x => x.Numero).NotEmpty().MinimumLength(6).MaximumLength(8).Matches("^[0-9]*$").WithMessage("El número no es válido");
-    //    }
-    //}
 }
